3c175b8257160b7e77b79bf5c84c69f4f693895b
Jonas <jonas.prell@hsbi.de>
2025-06-26 11:25:00 +0200

    Last change

--- tailwindproject/src/App.jsx
+++ tailwindproject/src/App.jsx
@@ -2,7 +2,7 @@ import './App.css';
 
 const App = () => {
   return <div>
-    <section className="h-screen bg-green-400">
+    <section className="h-screen bg-green-400 flex items-center justify-center text-center text-3xl m-12">
       <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aperiam adipisci blanditiis ullam voluptatum, facilis dolorum nobis! Sunt veritatis tenetur non, itaque exercitationem perferendis debitis voluptatibus aspernatur ipsa sint, vero expedita.</p>
     </section>
   </div>;
